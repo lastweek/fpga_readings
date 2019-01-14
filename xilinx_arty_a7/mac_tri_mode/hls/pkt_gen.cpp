56a0f8d460813b416f41f17df4e3719d6b82ac0d
Yizhou Shan <ys@purdue.edu>
2019-01-13 22:21:13 -0500

    merge my pkt gen in tri mode ref design

--- xilinx_arty_a7/mac_tri_mode/hls/pkt_gen.cpp
+++ xilinx_arty_a7/mac_tri_mode/hls/pkt_gen.cpp
@@ -6,67 +6,41 @@
 #include "ap_axi_sdata.h"
 #include "hls_stream.h"
 #include "string.h"
-
-/* including header */
-#define ETH_MIN_LEN	64
-#define ETH_MAX_LEN 1500
-
-struct eth_packet {
-	char dst[6];
-	char src[6];
-	char len_type[2];
-	char data[50];
-};
-
-struct my_axis{
-    char             data;
-    ap_uint<1>       last;
-};
-
-
-#define HDR_SIZE	14
-#define PKT_SIZE	64
-
-static inline void init_eth_packet(struct eth_packet *pkt)
-{
-	int i;
-	char *p;
-
-	p = (char *)pkt;
-	for (i = 0; i < 64; i++) {
-		p[i] = i;
-	}
-
-	pkt->len_type[0] = 0x40;
-	pkt->len_type[1] = 0;
-}
+#include "pkt_gen.h"
 
 void pkt_gen(hls::stream<my_axis> *output)
 {
 #pragma HLS INTERFACE ap_ctrl_none port=return
 #pragma HLS INTERFACE axis port=output
 
-	int i, j;
+	int i, j, len;
 	struct my_axis foo;
-	char *p;
-	static struct eth_packet pkt;
+	unsigned char p[MAX_PKT_SIZE];
 
-	init_eth_packet(&pkt);
+	/* Fill with random data */
+	for (i = 0; i < MAX_PKT_SIZE; i++) {
+		p[i] = i % 256;
+	}
 
-	p = (char *)&pkt;
+	for (i = 0; i < NR_LOOPS; i++) {
+		for (len = MIN_PKT_SIZE; len < MAX_PKT_SIZE; len += PKT_SIZE_STEP) {
+			/* Update the length field*/
+			p[12] = (unsigned char)(len >> 8);
+			p[13] = (unsigned char)len;
 
-	LOOP_SEND: for (j = 0; j < 10; j++) {
-		LOOP_SEND_ONE: for (i = 0; i < PKT_SIZE ; i++) {
-		#pragma HLS UNROLL
+			/* This loop sends out one packet */
+			LOOP_PACKET: for (j = 0; j < len ; j++) {
+			#pragma HLS PIPELINE
+				foo.data = p[j];
 
-			foo.data = p[i];
+				/* Raise axi_last for last byte */
+				if (j == (len - 1))
+					foo.last = 1;
+				else
+					foo.last = 0;
 
-			if (i == (PKT_SIZE - 1))
-				foo.last = 1;
-			else
-				foo.last = 0;
-
-			output->write(foo);
+				output->write(foo);
+			}
 		}
 	}
 }
